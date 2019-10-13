fe8437c388a058c83c39eb66215211e5428df74d
Shon Feder <shon.feder@gmail.com>
2019-10-13 13:25:52 -0400

    Clean up API of Crosswalk

--- align/src/crosswalk.ml
+++ align/src/crosswalk.ml
@@ -4,11 +4,10 @@ module type S = sig
   include Alg.Functor.S
   include Alg.Cata.S with type 'a t := 'a t
 
-  module Align : Align.S
-  type 'a f = 'a Align.t
+  type 'a f
 
-  val walk : f:('a -> 'b Align.t) -> 'a t -> 'b t Align.t
-  val sequence : 'a Align.t t -> 'a t Align.t
+  val walk : f:('a -> 'b f) -> 'a t -> 'b t f
+  val sequence : 'a f t -> 'a t f
 end
 
 module type Seed = sig
@@ -21,56 +20,46 @@ module type Seed = sig
   val walk : f:('a -> 'b Align.t) -> 'a t -> 'b t Align.t
 end
 
-module Make (Seed : Seed) : S
-  with module Align = Seed.Align
-   and type 'a t = 'a Seed.t
-   and type 'a f = 'a Seed.Align.t
-= struct
+module Make (Seed : Seed) = struct
   include Seed
 
   let sequence t = Seed.walk ~f:Fun.id t
 end
 
-module Option (A : Align.S) : S
-  with module Align = A
-   and type 'a t = 'a Option.t
-   and type 'a f = 'a A.t
-= struct
-  module Seed = struct
-    include Alg.Functor.Option
-    include Alg.Cata.Option
+module Option = struct
+  module Make (A : Align.S) = struct
+    module Seed = struct
+      include Alg.Functor.Option
+      include Alg.Cata.Option
 
-    module Align = A
-    type 'a f = 'a Align.t
+      module Align = A
+      type 'a f = 'a Align.t
 
-    let walk ~f = function
-      | None -> A.empty
-      | Some a -> A.map ~f:Option.some (f a)
+      let walk ~f = function
+        | None -> A.empty
+        | Some a -> A.map ~f:Option.some (f a)
+    end
+
+    include Make (Seed)
   end
-
-  include Make (Seed)
 end
 
-module List (A : Align.S) : S
-  with module Align = A
-   and type 'a t = 'a List.t
-   and type 'a f = 'a A.t
-= struct
-  module Seed = struct
-    include Alg.Functor.List
-    include Alg.Cata.List
+module List = struct
+  module Make (A : Align.S) = struct
+    module Seed = struct
+      include Alg.Functor.List
+      include Alg.Cata.List
 
-    module Align = A
-    type 'a f = 'a Align.t
+      module Align = A
+      type 'a f = 'a Align.t
 
-    let rec walk ~f t =
-      let cons_these th = These.case (fun x -> [x]) Fun.id (fun x xs -> x :: xs) th in
-      match t with
-      | []      -> Align.empty
-      | x :: xs -> Align.align_with ~f:cons_these (f x) (walk ~f xs)
+      let rec walk ~f t =
+        let cons_these th = These.case (fun x -> [x]) Fun.id (fun x xs -> x :: xs) th in
+        match t with
+        | []      -> Align.empty
+        | x :: xs -> Align.align_with ~f:cons_these (f x) (walk ~f xs)
+    end
+
+    include Make (Seed)
   end
-
-  include Make (Seed)
 end
-
-(** TODO Array *)
--- align/src/crosswalk.mli
+++ align/src/crosswalk.mli
@@ -1,25 +1,34 @@
+module Align = Align_internal
+
 module type S = sig
   include Alg.Functor.S
   include Alg.Cata.S with type 'a t := 'a t
 
-  module Align : Align_internal.S
-  type 'a f = 'a Align.t
+  type 'a f
 
-  val walk : f:('a -> 'b Align.t) -> 'a t -> 'b t Align.t
-  val sequence : 'a Align.t t -> 'a t Align.t
+  val walk : f:('a -> 'b f) -> 'a t -> 'b t f
+  val sequence : 'a f t -> 'a t f
 end
 
 module type Seed = sig
   include Alg.Functor.S
   include Alg.Cata.S with type 'a t := 'a t
 
-  module Align : Align_internal.S
+  module Align : Align.S
   type 'a f = 'a Align.t
 
   val walk : f:('a -> 'b Align.t) -> 'a t -> 'b t Align.t
 end
 
-module Make (Seed : Seed) : S
-  with module Align = Seed.Align
-   and type 'a t = 'a Seed.t
-   and type 'a f = 'a Seed.Align.t
+module Make (Seed : Seed) : S with type 'a t = 'a Seed.t
+                               and type 'a f = 'a Seed.Align.t
+
+module Option : sig
+  module Make (A : Align.S) : S with type 'a t = 'a Option.t
+                                 and type 'a f = 'a A.t
+end
+
+module List : sig
+  module Make (A : Align.S) : S with type 'a t = 'a List.t
+                                 and type 'a f = 'a A.t
+end
